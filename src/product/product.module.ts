1170015ca4cb501143a59b77c6bb0947aa5c61e8
wellington <wellingtonfds@gmail.com>
2024-02-25 18:20:24 -0300

    update schema

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -3,9 +3,10 @@ import { DatabaseModule } from 'src/database/database.module';
 import { ProductController } from './product.controller';
 import { ProductResolver } from './product.resolver';
 import { ProductService } from './product.service';
+import { QuestionModule } from './question/question.module';
 
 @Module({
-    imports: [DatabaseModule],
+    imports: [DatabaseModule, QuestionModule],
     controllers: [ProductController],
     providers: [ProductService, ProductResolver]
 })
