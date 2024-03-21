bd9cc344f2cbc0bb8e5c690519f7d5c0074ec937
wellington <wellingtonfds@gmail.com>
2024-03-20 21:00:06 -0300

    create signUp

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,14 @@ import { Module } from "@nestjs/common";
 import { GraphQLModule } from "@nestjs/graphql";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { AuthModule } from './auth/auth.module';
 import { CompanyModule } from './company/company.module';
 import { DatabaseModule } from "./database/database.module";
 import { ProductCategoryModule } from './product-category/product-category.module';
 import { ProductModule } from "./product/product.module";
 import { BigIntScalar } from "./scalars";
-import { UserModule } from './user/user.module';
 import { SignupModule } from './signup/signup.module';
-import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [DatabaseModule, ProductModule,
@@ -20,6 +20,7 @@ import { AuthModule } from './auth/auth.module';
       sortSchema: true,
       introspection: true,
       resolvers: { BigInt: BigIntScalar },
+
 
     }),
     UserModule,
